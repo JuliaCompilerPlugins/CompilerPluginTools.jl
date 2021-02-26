4e301e81feb3258e8836bb8bc56aa95bc6beed82
Documenter.jl <documenter@juliadocs.github.io>
2021-02-26 23:48:31 +0000

    build based on 5adba42

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Yuan","category":"page"},{"location":"#Yuan","page":"Home","title":"Yuan","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Yuan]","category":"page"}]
+}
